d0a5a99533e32172420919d6f308fbc136a7a183
Kimbro Staken <kstaken@terascope.io>
2019-04-12 14:06:56 -0700

    adding support for node.js sort and fixing the equals case in the sorting alg.

--- asset/__lib/utils.js
+++ asset/__lib/utils.js
@@ -4,10 +4,17 @@ const _ = require('lodash');
 
 module.exports = {
     sortFunction: (field, order) => {
-        const sortDescending = (a, b) => (_.get(a, field) < _.get(b, field) ? 1 : -1);
+        const sortDescending = (a, b) => {
+            if (_.get(a, field) === _.get(b, field)) return 0;
+            return (_.get(a, field) < _.get(b, field) ? 1 : -1);
+        };
 
         // Default to ascending
-        let sort = (a, b) => (_.get(a, field) > _.get(b, field) ? 1 : -1);
+        let sort = (a, b) => {
+            if (_.get(a, field) === _.get(b, field)) return 0;
+            return (_.get(a, field) > _.get(b, field) ? 1 : -1);
+        };
+
         if (order === 'desc') sort = sortDescending;
 
         return sort;
