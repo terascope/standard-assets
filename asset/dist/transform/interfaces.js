b004945b45842f8ab81c00e2171524f174a67537
Jared Noble <jsnoble1@gmail.com>
2019-10-10 10:17:11 -0700

    remove dist folder

--- asset/dist/transform/interfaces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NotifyType;
-(function (NotifyType) {
-    NotifyType["matcher"] = "matcher";
-    NotifyType["extraction"] = "extraction";
-})(NotifyType = exports.NotifyType || (exports.NotifyType = {}));
-//# sourceMappingURL=interfaces.js.map
