8b8977439a1b5676ece82e93f538451364e842f2
Charlie Iorg <ciorg@terascope.io>
2021-08-11 15:34:08 -0700

    bumped asset version to 0.12.3

--- asset/src/date_router/schema.ts
+++ asset/src/date_router/schema.ts
@@ -10,7 +10,7 @@ export default class Schema extends ConvictSchema<DateRouterConfig> {
                 format: 'required_String'
             },
             resolution: {
-                doc: 'Type of time series data: yearly, monthly, weekly, daily.  Weekly is weeks since start of epoch time',
+                doc: 'Type of time series data: yearly, monthly, weekly, daily.  Weekly is weeks since start of epoch time, Jan 1, 1970',
                 default: DateResolution.daily,
                 format: Object.keys(DateResolution)
             },
