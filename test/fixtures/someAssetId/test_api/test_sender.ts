faaadb2602b041010e7c8ce8e82ccd80817b758f
peterdemartini <thepeterdemartini@gmail.com>
2021-11-10 12:37:35 -0700

    Fix the test_sender interface

--- test/fixtures/someAssetId/test_api/test_sender.ts
+++ test/fixtures/someAssetId/test_api/test_sender.ts
@@ -9,9 +9,10 @@ export default class TestSenderApi implements RouteSenderAPI {
         this.configArgs.push(...args);
     }
 
-    async send(...args: any[]): Promise<void> {
+    async send(...args: any[]): Promise<number> {
         this.sendArgs.push(...args);
         await pDelay(Math.random() * 10);
+        return Array.isArray(args[0]) ? args[0].length : -1;
     }
 
     async verify(...args:any[]): Promise<void> {
