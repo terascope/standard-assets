5bcae85b48945cea73284c0f995f8a254eb690b9
Jeff Montagna <jeff.montagna@gmail.com>
2023-05-03 16:28:51 -0700

    removed initialize call from CountByField constructor and removed hard coded op name

--- asset/src/count_by_field/processor.ts
+++ asset/src/count_by_field/processor.ts
@@ -12,15 +12,13 @@ export default class CountByField extends MapProcessor<CountByFieldConfig> {
     constructor(context: WorkerContext, opConfig: CountByFieldConfig,
         exConfig: ExecutionConfig) {
         super(context, opConfig, exConfig);
-        this.opName = 'count_by_field';
-        this.initialize();
     }
 
     async initialize(): Promise<void> {
         if (this.opConfig.collect_metrics) {
             this.metrics = await this.createAPI(this.opConfig.metric_api_name);
-            const name = `${this.opName}_count_total`;
-            const help = `${this.opName} value field count`;
+            const name = `${this.opConfig._op}_count_total`;
+            const help = `${this.opConfig._op} value field count`;
             const labelNames = ['value', 'field', 'op_name'];
             const type = 'counter';
             await this.metrics.addMetric(name, help, labelNames, type);
@@ -32,9 +30,9 @@ export default class CountByField extends MapProcessor<CountByFieldConfig> {
             const metricLabels = {
                 value: doc[this.opConfig.field],
                 field: this.opConfig.field,
-                op_name: this.opName
+                op_name: this.opConfig._op
             };
-            this.metrics.inc(`${this.opName}_count_total`, metricLabels, 1);
+            this.metrics.inc(`${this.opConfig._op}_count_total`, metricLabels, 1);
         }
         return doc;
     }
