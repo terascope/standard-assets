41c1b10ed33c3b3160ff84802405da0f44b7adfb
busma13 <pluitjens@terascope.io>
2025-05-09 16:36:35 -0700

    add tests

--- asset/src/sample_exact_es_percent/interfaces.ts
+++ asset/src/sample_exact_es_percent/interfaces.ts
@@ -1,8 +1,12 @@
-import { OpConfig } from '@terascope/types';
+import { ESLikeClient as ES, OpConfig } from '@terascope/types';
 
 export interface SampleExactESPercentConfig extends OpConfig {
     connection: string;
     index: string;
     document_id: string;
     window_ms: number;
+}
+
+export interface Client {
+    get: (query: ES.GetParams, fullResponse?: boolean) => Promise<ES.GetResponse>;
 }
