6bb4e6f7b068a5fbc8bcec648d9e340f12bc7d35
busma13 <pluitjens@terascope.io>
2026-04-15 09:58:59 -0700

    data_generator get size from sender api, not op

--- asset/src/data_generator/slicer.ts
+++ asset/src/data_generator/slicer.ts
@@ -1,4 +1,4 @@
-import { get } from '@terascope/core-utils';
+import { get, isKey } from '@terascope/core-utils';
 import { Slicer, SlicerRecoveryData } from '@terascope/job-components';
 import { DataGenerator, CounterResults } from './interfaces.js';
 import Counter from './counter.js';
@@ -10,17 +10,25 @@ export default class DataGeneratorSlicer extends Slicer<DataGenerator> {
         await super.initialize(recoveryData);
 
         const { size } = this.opConfig;
-        const isPersistent = this.executionConfig.lifecycle === 'persistent';
+        const { lifecycle, operations, apis } = this.executionConfig;
+        const isPersistent = lifecycle === 'persistent';
         let alreadyProcessed: undefined | number;
 
         if (this.recoveryData) {
             alreadyProcessed = get(this.recoveryData[0], 'lastSlice.processed', 0);
         }
 
-        const opListSize = this.executionConfig.operations.length - 1;
-        const lastOp = this.executionConfig.operations[opListSize];
+        let lastOpSize;
+        const opListSize = operations.length - 1;
+        const lastOpApiName = operations[opListSize]._api_name;
+        if (lastOpApiName !== undefined) {
+            const lastOpApi = apis.filter((api) => api._name = lastOpApiName)[0];
+            if (isKey(lastOpApi, 'size')) {
+                lastOpSize = lastOpApi.size;
+            }
+        }
 
-        const counter = new Counter(isPersistent, size, lastOp.size, alreadyProcessed);
+        const counter = new Counter(isPersistent, size, lastOpSize, alreadyProcessed);
         this.countHandle = counter.handle;
     }
 
