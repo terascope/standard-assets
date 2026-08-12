5eea9ba2db9046a460d506f6206e753b7691ac39
Peter Luitjens <43619525+busma13@users.noreply.github.com>
2026-08-12 09:56:54 -0700

    Date router dead letter default (#1248)
    This PR makes the following change:
    - The schema for the `DateRouterProcessor` operation now includes the
    `_dead_letter_action` field with a default value of `log`. This will
    override the default value of `throw` that is set by the `opSchema` in
    job-components when the schemas are merged during opConfig validation.
    
    The `DateRouterProcessor` tries to validate the date before it is used
    to create the route metadata. If validation fails the function will
    throw. Because the processor is a `MapProcessor`, when set to `throw`
    the whole map function will fail, the slice will error and and no
    records from the slice will be processed. When set to `log` the error is
    caught, an error level log is printed showing the `Bad record` and the
    map function continues with the rest of the records for that slice. The
    bad record is replaced with `null`, which will cause an error on the
    `RoutedSender` (no metadata to parse), so that processor must also set
    the `_dead_letter_action` to not `throw` or we run into a similar
    problem.
    
    Ref: #1246

--- asset/src/date_router/schema.ts
+++ asset/src/date_router/schema.ts
@@ -33,6 +33,19 @@ export default class Schema extends BaseSchema<DateRouterConfig> {
                 doc: 'determines if the date unit (year, month, day) should be included in final output',
                 default: false,
                 format: 'Boolean'
+            },
+            _dead_letter_action: {
+                doc: [
+                    'This action will specify what to do when failing to parse or transform a record.',
+                    'The following builtin actions are supported:',
+                    '  - "throw": throw the original error​​',
+                    '  - "log": (default) log the error and the data​​',
+                    '  - "none": skip the error entirely',
+                    'If none of the actions are specified it will try to use a registered Dead Letter Queue API under that name.',
+                    'The API must already be created by an operation before it can be used.'
+                ].join('\n'),
+                default: 'log',
+                format: 'optional_string'
             }
         };
     }
